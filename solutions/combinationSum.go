310eb64dc5e5101913b8ceddedf1c09825d162f0
Vipul Sharma <56762266+VipulSharma1999@users.noreply.github.com>
2023-12-02 19:22:39 +0530

    Create combinationSum.go

---
+++ solutions/combinationSum.go
@@ -0,0 +1,28 @@
+package main
+
+func combinationSum(candidates []int, target int) [][]int {
+	var ans [][]int
+	if len(candidates) == 0 {
+		return ans
+	}
+
+	backtracking(&ans, make([]int, 0), candidates, 0, target)
+
+	return ans
+}
+
+func backtracking(ans *[][]int, temp, candidates []int, i, target int) {
+	if target < 0 || i > len(candidates) {
+		return
+	}
+	if target == 0 {
+		ctemp := make([]int, len(temp))
+		copy(ctemp, temp)
+		*ans = append(*ans, ctemp)
+	}
+	for j := i; j < len(candidates); j++ {
+		temp = append(temp, candidates[j])
+		backtracking(ans, temp, candidates, j, target-candidates[j])
+		temp = temp[:len(temp)-1]
+	}
+}
